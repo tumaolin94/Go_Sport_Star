3317632a955435e150b247b3733f277e7df2a5ab
Maolin Tu <tumaolin94@gmail.com>
2019-06-01 14:57:19 -0700

    update dao

--- dao/superstar_dao.go
+++ dao/superstar_dao.go
@@ -1,7 +1,67 @@
 package dao
 
-import "github.com/go-xorm/xorm"
+import (
+	"github.com/go-xorm/xorm"
+	"log"
+	"superstar/models"
+)
 
 type SuperStarDao struct {
 	engine *xorm.Engine
+}
+
+func NewSuperstarDao(engine *xorm.Engine) *SuperStarDao {
+	return &SuperStarDao{
+		engine:engine,
+	}
+}
+
+func (d *SuperStarDao) Get(id int) *models.StarInfo {
+	data := &models.StarInfo{Id:id}
+	ok, err := d.engine.Get(data)
+	if ok && err == nil {
+		return data
+	} else {
+		return nil
+		data.Id = 0
+		return data
+	}
+}
+
+func (d *SuperStarDao) GetAll() []models.StarInfo {
+	datalist := []models.StarInfo{}
+	err := d.engine.Desc("id").Find(&datalist)
+	if err != nil {
+		log.Print(err)
+		return datalist
+	} else {
+		return datalist
+	}
+}
+
+func (d *SuperStarDao) Search(country string) []models.StarInfo {
+	datalist := []models.StarInfo{}
+	err := d.engine.Where("country=?", country).Desc("id").Find(&datalist)
+	if err != nil {
+		log.Print(err)
+		return datalist
+	} else {
+		return datalist
+	}
+}
+
+func (d *SuperStarDao) Delete(id int) error {
+	data := &models.StarInfo{Id:id, SysStatus:1}
+	_, err := d.engine.Id(data.Id).Update(data)
+	return err
+}
+
+func (d *SuperStarDao) Update(data *models.StarInfo, columns []string) error {
+	_, err := d.engine.Id(data.Id).MustCols(columns...).Update(data)
+	return err
+}
+
+func (d *SuperStarDao) Create(data *models.StarInfo) error {
+	_, err := d.engine.Insert(data)
+	return err
 }
