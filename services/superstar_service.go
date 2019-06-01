7390e2d178bf8f7b386a2dc4b37c6afaecf121b3
Maolin Tu <tumaolin94@gmail.com>
2019-06-01 15:09:54 -0700

    superstar_service

--- services/superstar_service.go
+++ services/superstar_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"superstar/dao"
+	"superstar/datasource"
 	"superstar/models"
 )
 
@@ -12,13 +14,44 @@ import (
 // because we may need to change or try an experimental different domain logic at the future.
 type SuperstarService interface {
 	GetAll() []models.StarInfo
-	Get(id int) models.StarInfo
-	Delete(id int) bool
+	Get(id int) *models.StarInfo
+	Delete(id int) error
 
-	Update(user *models.StarInfo) error
+	Update(user *models.StarInfo, columns []string) error
 
 	Create(user *models.StarInfo) error
 
 	Search(country string) []models.StarInfo
+}
+
+type superstarService struct {
+	dao *dao.SuperStarDao
+
+}
+func NewSuperstarService() SuperstarService {
+	return &superstarService {
+		dao: dao.NewSuperstarDao(datasource.InstanceMaster()),
+	}
+}
+func (s *superstarService) GetAll() []models.StarInfo {
+	return s.dao.GetAll()
+}
+func (s *superstarService) Get(id int) *models.StarInfo {
+	return s.dao.Get(id)
+}
+func (s *superstarService) Delete(id int) error {
+	return s.dao.Delete(id)
+}
+
+func (s *superstarService) Update(user *models.StarInfo, columns []string) error {
+	return s.dao.Update(user, columns)
+}
+
+func (s *superstarService) Create(user *models.StarInfo) error {
+	return s.dao.Create(user)
+}
+
+func (s *superstarService) Search(country string) []models.StarInfo {
+	return s.dao.Search(country)
 }
 
